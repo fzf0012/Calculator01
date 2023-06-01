edc8dd1b07c615fe0e0d04a11255f15fd8b4dcbb
Frank <fzf0012@auburn.edu>
2023-06-01 10:56:40 -0400

    initial design

--- Calculator01/ViewController.swift
+++ Calculator01/ViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var displayLabel: UILabel!
+    private let buttons = [
+        ["C", "+/-", "%", "/"],
+        ["7", "8", "9", "*"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", "","="]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
     }
 
+    private func setupUI() {
+        displayLabel = UILabel()
+        displayLabel.translatesAutoresizingMaskIntoConstraints = false
+        displayLabel.textAlignment = .right
+        displayLabel.font = UIFont.systemFont(ofSize: 50)
+        displayLabel.text = "0"
+        view.addSubview(displayLabel)
 
+        let mainStackView = UIStackView()
+        mainStackView.axis = .vertical
+        mainStackView.distribution = .fillEqually
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mainStackView)
+        
+        NSLayoutConstraint.activate([
+            displayLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            displayLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            displayLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            displayLabel.heightAnchor.constraint(equalToConstant: 100),
+            
+            mainStackView.topAnchor.constraint(equalTo: displayLabel.bottomAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+        for buttonRow in buttons {
+            let rowStackView = UIStackView()
+            rowStackView.axis = .horizontal
+            rowStackView.distribution = .fillEqually
+            for buttonTitle in buttonRow {
+                let button = UIButton()
+                button.setTitle(buttonTitle, for: .normal)
+                button.setTitleColor(.black, for: .normal)
+                button.backgroundColor = .lightGray
+                button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+                rowStackView.addArrangedSubview(button)
+            }
+            mainStackView.addArrangedSubview(rowStackView)
+        }
+    }
 }
-
